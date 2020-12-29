bae41a5904c0f5469bd17f552422da98de835659
jasonthealmighty@gmail.com <jasonthealmighty@gmail.com>
2020-12-28 18:29:01 -0700

    worked on list

--- app/Models/List.js
+++ app/Models/List.js
@@ -5,4 +5,23 @@ export default class List{
         this.title = title
         this.listId = listId || generateId()
     }
+    get Template{
+        return /*html*/ `
+            <div class="row">
+            <div class="col-4">    
+                <div class="card border-primary">
+                    <h5 class="card-header bg-primary">List Title</h5>
+                    <div class="card-body">
+                        <p class="card-text">a list thing goes here</p>
+                        <p class="card-text">a list thing goes here</p>
+                        <p class="card-text">a list thing goes here</p>
+                    </div>
+                    <div class="form-inline align-self-center py-3">
+                        <input type="text" class="form-control" id="listItem" placeholder="Add Task" required>
+                        <button type="submit" class="btn btn-outline-info" >+</button>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    }
 }
